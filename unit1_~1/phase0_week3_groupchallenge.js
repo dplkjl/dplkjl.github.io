c844a947ae58fd144fbaff605737b8cfa168c4ab
Karen Larsen <dplkaren@gmail.com>
2014-06-20 09:59:39 +1200

    replacement group challenge

--- unit1_~1/phase0_week3_groupchallenge.js
+++ unit1_~1/phase0_week3_groupchallenge.js
@@ -1,15 +1,4 @@
 //-------------------------------------------------------
-// My role in the group is:
-// Story Teller
-// Here is my part of the challenge:
-
-// What I want:
-// I have in my possession a bunch of numbers.
-// I want to add them all together.
-// I also want to find their average.
-// Finally, I want to be able to to find the median number 
-// of that group of numbers. 
-//-------------------------------------------------------
 //-------------------------------------------------------
 // My role in the group is:
 // pseudo coder
